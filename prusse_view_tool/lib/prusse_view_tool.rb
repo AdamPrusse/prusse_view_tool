0cc94959999ac98bd006f53f3932a972a477708d
Adam Prusse <adamprusse@Adams-MBP.ad.uvu.edu>
2017-12-12 10:00:30 -0700

    Dev tool initial commit

---
+++ prusse_view_tool/lib/prusse_view_tool.rb
@@ -0,0 +1,6 @@
+require "prusse_view_tool/version"
+require "prusse_view_tool/renderer"
+
+module PrusseViewTool
+  # Your code goes here...
+end
---
+++ prusse_view_tool/lib/prusse_view_tool/version.rb
@@ -0,0 +1,3 @@
+module PrusseViewTool
+  VERSION = "0.1.0"
+end
